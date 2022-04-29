d299cf652790e70e707957b6c994a5d1f7b2cf32
Galuh Apriliano <galuhapriliano30@gmail.com>
2022-04-29 22:31:06 +0700

    Add files via upload

---
+++ minggu10/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:petani/main.dart';
+import 'package:petani/minggu10/get_data.dart';
+
+void main() {
+  runApp(
+    MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Get API',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: GetDataScreen(),
+    );
+  }
+}
